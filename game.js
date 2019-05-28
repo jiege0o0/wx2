ed24cb67acf9fedb07ee93f4a605c09b36e589fc
caiweihan <caiweihan@local.host>
2019-05-28 15:51:54 +0800

    1

--- game.js
+++ game.js
@@ -17,6 +17,7 @@ if(window.RES && RES.processor) {
     require('./library/binary.js');
 }
 require('./utils/wlad_sdk_game.min.js');
+require('./utils//ald-game.js')
 
 
 // var fundebug = require("./library/fundebug.0.4.0.min.js");
---
+++ utils/ald-game-conf.js
@@ -0,0 +1,2 @@
+exports.app_key = "89591d6fb87d5d538453cec0bc9af086"; //请在此行填写从阿拉丁后台获取的appkey
+exports.getLocation = false; //默认不获取用户坐标位置
